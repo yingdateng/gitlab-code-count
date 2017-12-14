8cb8edcedd4475923f04e8f5ce5df086403bdd3d
James Ward <james@jamesward.org>
2017-12-14 13:28:29 -0700

    proper sample

--- src/main/java/org/webjars/samplespring_boot/WebApplication.java
+++ src/main/java/org/webjars/samplespring_boot/WebApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SampleSpringBootApplication {
+public class WebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
 }
